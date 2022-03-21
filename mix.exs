a5b9e3da32fe27e9e3f2ee030e317c15848223ad
Marcos Soares <mavmaso@gmail.com>
2022-03-21 12:23:59 -0300

    add ex_machina

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Posterr.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_machina, "~> 2.7.0", only: :test}
     ]
   end
 
